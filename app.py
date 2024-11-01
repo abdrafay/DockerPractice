c8871e15a942efb1093872df95548da2ac852485
abdrafay <zainrafay12@gmail.com>
2024-11-01 15:53:57 +0500

    All Done, Testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! now its live from github actions and docker'
+    return 'Hello, World! Rider Hasnain'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
