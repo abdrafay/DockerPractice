b1ba3d6dc1457c7c1e46335dde18185c1380f8f4
abdrafay <zainrafay12@gmail.com>
2024-11-07 21:51:24 +0500

    Testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! Checking ci/cd pipeline'
+    return 'Hello, World! Checking ci/cd pipeline checking if everythign works perfectly'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
